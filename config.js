91c13596b342187589c1ddfab9e076b6e1c06e12
otienoj100 <otienoj@gmail.com>
2026-07-09 11:46:39 +0300

    Updated config.js file

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Supabase Dashboard → Project Settings → API
 // ============================================================
 window.SUPABASE_URL = "yvkymgjsdlsbajnwlgiy";       // e.g. https://xxxxxxxx.supabase.co
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2a3ltZ2pzZGxzYmFqbndsZ2l5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzU1NTg0OCwiZXhwIjoyMDk5MTMxODQ4fQ.mt0sQAM5EnM6CZN4zLyHKvOZFZX3JbKAYXtHJUBsE7g";  // the "anon" / "public" key, NOT the service_role key
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2a3ltZ2pzZGxzYmFqbndsZ2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1NTU4NDgsImV4cCI6MjA5OTEzMTg0OH0.p5rz9UHBZCUAJKIFPQOcbBM3wEaGjATa84r1PJYugls"
